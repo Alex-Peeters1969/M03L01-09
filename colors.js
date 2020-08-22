f4e4afd3789ab49c86b2fe4eb111e454044f4703
Alex-Peeters1969 <67190405+Alex-Peeters1969@users.noreply.github.com>
2020-08-22 14:15:48 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,26 @@
+/*let colors = ['yellow', 'blue', 'red', 'orange'];
+    let i = 0;
+    while (i < colors.length){
+    console.log (colors [i]);
+
+    i++;
+    }
+for (let i = 0; i < Array.length; i++){
+    console.log ( 'colors printed');
+}*/
+const colors = ['yellow', 'blue', 'red', 'orange'];
+
+colors.forEach(element => console.log(element));
+
+//1. 9 regels voor while en for loop.
+//2. 2 regels.
+//3. uitkomst beschrijf je in 1 regel.
+//4. 
+
+
+const myColors = ["white", "green", "gray", "brown", 'black'];
+    
+    for (x in myColors) {
+    console.log(myColors[x]);
+  }
+
